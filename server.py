cddd3894dc69f5a368afaa135e1a340e23ba699e
Kenny Yu <yu.kenny124@gmail.com>
2012-12-26 22:46:56 -0500

    created documentation for the web socket handler

--- server.py
+++ server.py
@@ -6,19 +6,20 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
-# Dictionary mapping id -> handler
+# Dictionary mapping currently online users id -> handler
 clients = {}
 
+# Message types that can be sent over the web socket
 MESSAGE_TYPES = {
-    "NEW_USER" : 0,
-    "MESSAGE" : 1,
-    "LEAVE_USER" : 2,
-    "ONLINE_USERS" : 3,
+    "NEW_USER" : 0,     # will include a "username" field
+    "MESSAGE" : 1,      # will include a "username" and "text" field
+    "LEAVE_USER" : 2,   # will include a "username" field
+    "ONLINE_USERS" : 3, # will include a "users" field
 }
 
 class ChatWebSocketHandler(tornado.websocket.WebSocketHandler):
     """
-    Handles chat websockets.
+    Handles chat web sockets connections
     """
 
     # Create a unique id per web socket
@@ -26,12 +27,19 @@ class ChatWebSocketHandler(tornado.websocket.WebSocketHandler):
     lock = threading.Lock()
 
     def open(self):
+        # Atomically set the current connection's id and increment the global
+        # counter.
         with self.__class__.lock:
             self.id = self.__class__.id;
             self.__class__.id += 1
         clients[self.id] = self
+
+        # When the client first connects, send the new client a list of
+        # currently online users.
         usernames = []
         for cid in clients:
+            # The "username" field will not be set in between the client
+            # opens a web socket connection and enters a username.
             if hasattr(clients[cid], "username"):
                 usernames.append(clients[cid].username)
         message = {
@@ -44,12 +52,16 @@ class ChatWebSocketHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
         print "received message: " + message
         data = json.loads(message);
+        # TODO: should check if the username is already taken
         if data["type"] == MESSAGE_TYPES["NEW_USER"]:
             clients[self.id].username = data["username"]
+
+        # Relay messages to all other currently online users
         for cid in clients:
             clients[cid].write_message(message)
 
     def on_close(self):
+        # Delete this user from our list of currently online users
         del clients[self.id]
         print "Websocket %d closed" % self.id
 
